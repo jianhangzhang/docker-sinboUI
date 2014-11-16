d4dfed54420d8e56adebe05fd95637b4648720e0
jianhangzhang <jianhang_zhang@sina.com>
2014-11-16 17:31:59 +0800

    commit

--- docker-sinui.rb
+++ docker-sinui.rb
@@ -3,22 +3,40 @@ require 'sinatra/reloader'
 require 'http'
 require 'json'
 
-URL='http://10.1.2.11:4243'
+HOST='http://10.1.2.11:4243'
+
+helpers do
+  def delete_a_container(container_id)
+    HTTP.delete HOST+"/containers/#{container_id}"
+  end
+end
 
 get '/' do
   erb :home
 end
 
 get '/images' do
-  conn = HTTP.get URL + '/images/json'
+  conn = HTTP.get HOST + '/images/json'
   resp = conn.body.readpartial
   @resp_json = JSON.parse(resp)
   erb :images
 end
 
 get '/containers' do
-  conn = HTTP.get URL + '/containers/json?all=1'
+  conn = HTTP.get HOST + '/containers/json?all=1'
   resp = conn.body.readpartial
   @resp_json = JSON.parse(resp)
   erb :containers
+end
+
+get '/containers/delete' do
+  delete_a_container(x)
+end
+
+get '/create_a_container' do
+  erb :create_a_container
+end
+
+post '/create_a_container' do
+  conn = HTTP.get HOST + '/containers/create'
 end
